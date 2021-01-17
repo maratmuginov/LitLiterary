1a9c83a24cdf385dd03c993588c28a309e13c87d
maratmuginov <71664001+maratmuginov@users.noreply.github.com>
2021-01-17 08:27:28 -0800

    Update RepositoryTests.cs

--- source/LitLiterary/LitLiteracy.Tests/Services/RepositoryTests.cs
+++ source/LitLiterary/LitLiteracy.Tests/Services/RepositoryTests.cs
@@ -1,33 +1,13 @@
-﻿using System;
-using System.Threading.Tasks;
-using LitLiteracy.Tests.Mocks;
-using LitLiterary.Lib.Shared.Contracts.Services;
-using LitLiterary.Lib.Shared.Models;
 using NUnit.Framework;
 
 namespace LitLiteracy.Tests.Services
 {
     public class RepositoryTests
     {
-        private readonly IRepository<Guid, DomainObject> _repository;
-
-        public RepositoryTests()
-        {
-            _repository = new MockLibraryRepository();
-        }
-
         [SetUp]
         public void Setup()
         {
             
-        }
-
-        [Test]
-        public async Task CanDomainObjectBeAdded()
-        {
-            var domainObject = new DomainObject();
-            await _repository.CreateAsync(domainObject);
-            Assert.True(domainObject.Id != default);
         }
     }
 }
